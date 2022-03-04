7f54fec0ed0a289a4d607731adabe3ad4b64dfae
DisDias <75290504+DisDias@users.noreply.github.com>
2022-03-04 18:40:36 +0700

    unduh file

--- client/client_select.py
+++ client/client_select.py
@@ -4,17 +4,36 @@ import sys
 host = '127.0.0.1'
 port = 65435
 
+def receiveFile(conn, namaFile, sizeFile):
+    with open(namaFile, 'wb') as rf:
+        while True:
+            data = conn.recv(int(sizeFile))
+            if not data:
+                print('Selesai')
+                break
+            rf.write(data)
+    rf.close()
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    sys.stdout.write('>> ')
+    #sys.stdout.write('>> ')
+    print('>>', end=' ', flush=True)
 
     try:
         while True:
             message = sys.stdin.readline()
             s.send(bytes(message, 'utf-8'))
             received_data = s.recv(1024).decode('utf-8')
-            sys.stdout.write(received_data)
-            sys.stdout.write('>> ')
+            if received_data == 'File Tidak Ditemukan':
+                #sys.stdout.write(received_data)
+                print(received_data)
+            else:
+                namaFile, sizeFile = received_data.split(":")
+                receiveFile(s, namaFile, sizeFile)
+
+            #sys.stdout.write('>> ')
+            print('>>', ' ', flush=True)
 
     except KeyboardInterrupt:
         s.close()
--- server/server_select.py
+++ server/server_select.py
@@ -1,9 +1,27 @@
 import socket
 import select
 import sys
+import os
 
 host = '127.0.0.1'
 port = 65435
+
+def findFile(datasetFile, namaFile):
+    for file in datasetFile:
+        namaAja = os.path.splitext(file)
+        if namaAja[0].lower() == namaFile.lower():
+            return file
+    return False
+
+def sendFile(pathFile, conn):
+    with open(pathFile, "rb") as sf:
+        data = sf.read(1024)
+        while (data):
+            conn.send(data)
+            print ('Sent', repr(data))
+            data = sf.read(1024)
+    sf.close()
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -15,7 +33,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
         while True:
             read_ready, write_ready, exception = select.select(input_socket, [], [])
-        
+            dataset = "./dataset/"
+            datasetFile = []
+
+            for (pathTemp, namaTemp, namaFile) in os.walk(dataset):
+                datasetFile.extend(namaFile)
+                break
+
             for sock in read_ready:
                 if sock == s:
                     client_socket, client_address = s.accept()
@@ -26,7 +50,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     print(sock.getpeername(), data)
                 
                     if data:
-                        sock.send(data)
+                        kata = data.decode("utf-8")
+                        splitKata = kata.split(" ")
+                        namaFile = splitKata[1].strip()
+                        resultFile = findFile(datasetFile, namaFile)
+                        if resultFile is False:
+                            sock.send(bytes('File Tidak Ditemukan', 'utf-8'))
+                        else:
+                            pathFile = dataset + resultFile
+                            sizeFile = os.path.getsize(pathFile)
+                            sock.send(f"{resultFile}:{sizeFile}".encode())
+                            sendFile(pathFile, sock)
+
                     else:                    
                         sock.close()
                         input_socket.remove(sock)
