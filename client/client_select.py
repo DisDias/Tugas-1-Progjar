1731cb5387e55c7fd2df200a5872695232f98176
Dyandra Paramitha <73653209+dydyandra@users.noreply.github.com>
2022-03-07 21:12:11 +0700

    add parsing header, fixed function for receivefile

--- client/client_select.py
+++ client/client_select.py
@@ -1,18 +1,33 @@
 import socket
 import sys
+import math
+import re
 
 host = '127.0.0.1'
 port = 65435
 
 def receiveFile(conn, namaFile, sizeFile):
+    max_loop = math.ceil(int(sizeFile) / 1024)
+    flag = 0
     with open(namaFile, 'wb') as rf:
         while True:
-            data = conn.recv(int(sizeFile))
-            if not data:
+            data = conn.recv(1024)
+            flag += 1
+            rf.write(data)
+            # print(flag, max_loop)
+            if max_loop == flag:
                 print('Selesai')
                 break
-            rf.write(data)
+            # rf.write(data)
     rf.close()
+
+def splitData(received_data):
+    namaFile, sizeFile = received_data.split(",\n")
+    title_name, namaFile = namaFile.split(":")
+    sizeFile = int(''.join(filter(str.isdigit, sizeFile)))
+    
+    return namaFile, str(sizeFile)
+    
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -28,8 +43,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if received_data == 'File Tidak Ditemukan':
                 #sys.stdout.write(received_data)
                 print(received_data)
+            elif received_data == 'Command yang dimasukkan salah. Silahkan masukkan unduh nama_file':
+                print(received_data)
             else:
-                namaFile, sizeFile = received_data.split(":")
+                # TO DO: cari parsing
+                namaFile, sizeFile = splitData(received_data)
+                print(namaFile, sizeFile)
+                # File-name: coba.txt 
+                # File-size:33\n\n\n
                 receiveFile(s, namaFile, sizeFile)
 
             #sys.stdout.write('>> ')
@@ -38,4 +59,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     except KeyboardInterrupt:
         s.close()
         sys.exit(0)
-
--- server/server_select.py
+++ server/server_select.py
@@ -6,10 +6,19 @@ import os
 host = '127.0.0.1'
 port = 65435
 
-def findFile(datasetFile, namaFile):
-    for file in datasetFile:
-        namaAja = os.path.splitext(file)
-        if namaAja[0].lower() == namaFile.lower():
+server_address = (host, port)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(server_address)
+server_socket.listen(5)
+
+input_socket = [server_socket]
+sourcePath = "./dataset/"
+
+def findFile(filename):
+    for file in os.listdir(sourcePath):
+        # namaAja = os.path.splitext(file)
+        if file.lower() == filename.lower():
             return file
     return False
 
@@ -18,54 +27,46 @@ def sendFile(pathFile, conn):
         data = sf.read(1024)
         while (data):
             conn.send(data)
-            print ('Sent', repr(data))
+            # data = sf.read(1024)
+            # print ('Sent', repr(data))
             data = sf.read(1024)
     sf.close()
 
+try:
+    while True:
+        read_ready, write_ready, exception = select.select(input_socket, [], [])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
-    s.listen(5)
-
-    input_socket = [s]
-
-    try:
-        while True:
-            read_ready, write_ready, exception = select.select(input_socket, [], [])
-            dataset = "./dataset/"
-            datasetFile = []
-
-            for (pathTemp, namaTemp, namaFile) in os.walk(dataset):
-                datasetFile.extend(namaFile)
-                break
-
-            for sock in read_ready:
-                if sock == s:
-                    client_socket, client_address = s.accept()
-                    input_socket.append(client_socket)        
-                
-                else:            	
-                    data = sock.recv(1024)
-                    print(sock.getpeername(), data)
-                
-                    if data:
-                        kata = data.decode("utf-8")
-                        splitKata = kata.split(" ")
-                        namaFile = splitKata[1].strip()
-                        resultFile = findFile(datasetFile, namaFile)
+        for sock in read_ready:
+            if sock == server_socket:
+                client_socket, client_address = server_socket.accept()
+                input_socket.append(client_socket)        
+            
+            else:            	
+                data = sock.recv(1024)
+                print(sock.getpeername(), data)
+            
+                if data:
+                    kata = data.decode("utf-8")
+                    splitKata = kata.split(" ")
+                    if splitKata[0] == 'unduh':
+                        filename = splitKata[1].strip()
+                        resultFile = findFile(filename)
                         if resultFile is False:
                             sock.send(bytes('File Tidak Ditemukan', 'utf-8'))
                         else:
-                            pathFile = dataset + resultFile
+                            pathFile = sourcePath + resultFile
                             sizeFile = os.path.getsize(pathFile)
-                            sock.send(f"{resultFile}:{sizeFile}".encode())
+                            # header = f"File-name:{resultFile},\nFile-size:{sizeFile}\n\n\n"
+                            sock.send(f"File-name:{resultFile},\nFile-size:{sizeFile}\n\n\n".encode())
                             sendFile(pathFile, sock)
+                    else:
+                        sock.send(bytes('Command yang dimasukkan salah. Silahkan masukkan unduh nama_file', 'utf-8'))
+                        
+                    
+                else:                    
+                    sock.close()
+                    input_socket.remove(sock)
 
-                    else:                    
-                        sock.close()
-                        input_socket.remove(sock)
-
-    except KeyboardInterrupt:        
-        s.close()
-        sys.exit(0)
+except KeyboardInterrupt:        
+    server_socket.close()
+    sys.exit(0)
